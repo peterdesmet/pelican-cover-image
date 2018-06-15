e7376128bf5310e49d9fa730ff6cd9506fd15649
Peter Desmet <peter.desmet.work@gmail.com>
2018-06-15 15:42:11 +0200

    Add cover_image_caption based on format described in settings

--- cover_image.py
+++ cover_image.py
@@ -2,8 +2,8 @@ from pelican import signals
 
 def cover_image(generator):
     """
-    Adds cover_image_url attribute for each article/page, based on cover_image in metadata or DEFAULT_COVER_IMAGE
-    Works for local images and URLs
+    Adds cover_image_url and cover_image_caption attributes to each article/page, based on 
+    metadata or pelican settings
     """
 
     # Create list of articles_or_pages
@@ -14,12 +14,12 @@ def cover_image(generator):
     articles_or_pages = articles + drafts + pages + hidden_pages
 
     for article_or_page in articles_or_pages:
-        # We use the cover_image from the article/page metadata if defined and not empty,
-        # otherwise we use the DEFAULT_COVER_IMAGE from the settings.
+        # Use cover_image from the article/page metadata if defined and not empty,
+        # otherwise use the COVER_IMAGE_DEFAULT from the settings.
         if hasattr(article_or_page, "cover_image") and article_or_page.cover_image:
             cover_image = article_or_page.cover_image
-        elif "DEFAULT_COVER_IMAGE" in generator.settings:
-            cover_image = generator.settings["DEFAULT_COVER_IMAGE"]
+        elif "COVER_IMAGE_DEFAULT" in generator.settings:
+            cover_image = generator.settings["COVER_IMAGE_DEFAULT"]
         else:
             cover_image = ""
 
@@ -35,8 +35,32 @@ def cover_image(generator):
         else:
             cover_image_url = "2"
 
-        # Add cover_image_url as attribute to article or page:
+
+        # Set cover_image_caption using COVER_IMAGE_CAPTION format and article/page metadata
+        if "COVER_IMAGE_CAPTION" in generator.settings:
+            cover_image_caption = generator.settings["COVER_IMAGE_CAPTION"]
+
+            # Match {...} pattern in caption, e.g. cover_image_ref and cover_image_by in
+            # "Image by <a href=\"{cover_image_ref}\">{cover_image_by}</a>"
+            import re
+            matches = re.findall(r"\{(.*?)\}", cover_image_caption)
+
+            # For each match, check if metadata attribute of same name is present and replace with
+            # its value in caption. If one of the attributes is missing, break and leave caption 
+            # empty (to avoid half-populated captions)
+            for match in matches:
+                metadata_attribute = getattr(article_or_page, match, False)
+                if metadata_attribute:
+                    cover_image_caption = cover_image_caption.replace("{" + match + "}", metadata_attribute)
+                else:
+                    cover_image_caption = ""
+                    break
+        else:
+            cover_image_caption = ""
+
+        # Add cover_image_url and cover_image_caption as attributes to article or page:
         article_or_page.cover_image_url = cover_image_url
+        article_or_page.cover_image_caption = cover_image_caption
 
 def register():
     signals.article_generator_finalized.connect(cover_image)
